32581cda3ddca638da1f70639f65b61317184630
chickenchobab <hew980812@gmail.com>
2025-11-19 22:52:55 +0900

    Implement button foundation

--- Source/Assassins/UI/Foundation/AssassinsButtonBase.cpp
+++ Source/Assassins/UI/Foundation/AssassinsButtonBase.cpp
@@ -2,4 +2,44 @@
 
 
 #include "UI/Foundation/AssassinsButtonBase.h"
+#include "CommonActionWidget.h"
 
+void UAssassinsButtonBase::SetButtonText(const FText& InText)
+{
+	bOverride_ButtonText = InText.IsEmpty();
+	ButtonText = InText;
+	RefreshButtonText();
+}
+
+void UAssassinsButtonBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	RefreshButtonText();
+}
+
+void UAssassinsButtonBase::UpdateInputActionWidget()
+{
+	Super::UpdateInputActionWidget();
+
+	RefreshButtonText();
+}
+
+void UAssassinsButtonBase::RefreshButtonText()
+{
+	if (bOverride_ButtonText || ButtonText.IsEmpty())
+	{
+		// Me: CommonButtonBase has input action widget.
+		if (InputActionWidget)
+		{
+			const FText ActionDisplayText = InputActionWidget->GetDisplayText();
+			if (!ActionDisplayText.IsEmpty())
+			{
+				UpdateButtonText(ActionDisplayText);
+				return;
+			}
+		}
+	}
+
+	UpdateButtonText(ButtonText);
+}
--- Source/Assassins/UI/Foundation/AssassinsButtonBase.h
+++ Source/Assassins/UI/Foundation/AssassinsButtonBase.h
@@ -13,5 +13,28 @@ class UAssassinsButtonBase : public UCommonButtonBase
 {
 	GENERATED_BODY()
 
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetButtonText(const FText& InText);
 
+protected:
+	//~UUserWidget interface
+	virtual void NativePreConstruct() override;
+	//~End of UUserWidget interface
+
+	//~UCommonButtonBase interface
+	virtual void UpdateInputActionWidget() override;
+	//~End of UCommonButtonBase interface
+
+	void RefreshButtonText();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void UpdateButtonText(const FText& InText);
+
+private:
+	UPROPERTY(EditAnywhere, Category="Button", meta=(InlineEditConditionToggle))
+	uint8 bOverride_ButtonText : 1;
+
+	UPROPERTY(EditAnywhere, Category="Button", meta=(EditCondition="bOverride_ButtonText"))
+	FText ButtonText;
 };
