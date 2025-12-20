88b4aefd4f42bf83d83fd245dc51eaf4fe2f9e12
chickenchobab <hew980812@gmail.com>
2025-12-20 18:02:18 +0900

    스킬 어빌리티 타입을 C++ 클래스가 아닌 블루프린트 클래스로 분류: 타겟팅과 논타겟팅이 있으며 각각은 즉시 시전과 사거리 내까지 이동 후 시전으로 나뉜다

--- Source/Assassins/AbilitySystem/Abilities/AssassinsGameplayAbility_LocationTargeted.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "AbilitySystem/Abilities/AssassinsGameplayAbility_LocationTargeted.h"
-
-UAssassinsGameplayAbility_LocationTargeted::UAssassinsGameplayAbility_LocationTargeted(const FObjectInitializer& ObjectInitializer)
-    : Super(ObjectInitializer)
-{
-}
