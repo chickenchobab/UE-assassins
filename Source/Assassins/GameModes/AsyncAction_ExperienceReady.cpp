983b2b21216de2b847316c812303d617c424ba48
chickenchobab <hew980812@gmail.com>
2026-01-08 00:06:25 +0900

    Experience 로드를 기다리는 async action 구현

---
+++ Source/Assassins/GameModes/AsyncAction_ExperienceReady.cpp
@@ -0,0 +1,88 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameModes/AsyncAction_ExperienceReady.h"
+#include "GameModes/AssassinsExperienceStateComponent.h"
+
+UAsyncAction_ExperienceReady::UAsyncAction_ExperienceReady(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+UAsyncAction_ExperienceReady* UAsyncAction_ExperienceReady::WaitForExperienceReady(UObject* InWorldContextObject)
+{
+	UAsyncAction_ExperienceReady* Action = nullptr;
+
+	if (UWorld* World = GEngine->GetWorldFromContextObject(InWorldContextObject, EGetWorldErrorMode::LogAndReturnNull))
+	{
+		Action = NewObject<UAsyncAction_ExperienceReady>();
+		Action->WorldPtr = World;
+		Action->RegisterWithGameInstance(World);
+	}
+
+	return Action;
+}
+
+void UAsyncAction_ExperienceReady::Activate()
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		if (AGameStateBase* GameState = World->GetGameState())
+		{
+			Step2_ListenToExperienceLoading(GameState);
+		}
+		else
+		{
+			World->GameStateSetEvent.AddUObject(this, &ThisClass::Step1_HandleGameStateSet);
+		}
+	}
+	else
+	{
+		// No world so we'll never finish naturally
+		SetReadyToDestroy();
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step1_HandleGameStateSet(AGameStateBase* GameState)
+{
+	if (UWorld* World = WorldPtr.Get())
+	{
+		World->GameStateSetEvent.RemoveAll(this);
+	}
+
+	Step2_ListenToExperienceLoading(GameState);
+}
+
+void UAsyncAction_ExperienceReady::Step2_ListenToExperienceLoading(AGameStateBase* GameState)
+{
+	check(GameState);
+	UAssassinsExperienceStateComponent* ExperienceComponent = GameState->FindComponentByClass<UAssassinsExperienceStateComponent>();
+	check(ExperienceComponent);
+
+	if (ExperienceComponent->IsExperienceLoaded())
+	{
+		UWorld* World = GameState->GetWorld();
+		check(World);
+
+		// The experience happened to be already loaded, but still delay a frame to
+		// make sure people don't write stuff that relies on this always being true
+		// Me: ???
+		World->GetTimerManager().SetTimerForNextTick(FTimerDelegate::CreateUObject(this, &ThisClass::Step4_BroadcastReady));
+	}
+	else
+	{
+		ExperienceComponent->CallOrRegister_OnExperienceLoaded(FOnAssassinsExperienceLoaded::FDelegate::CreateUObject(this, &ThisClass::Step3_HandleExperienceLoaded));
+	}
+}
+
+void UAsyncAction_ExperienceReady::Step3_HandleExperienceLoaded(const UAssassinsExperienceDefinition* CurrentExperience)
+{
+	Step4_BroadcastReady();
+}
+
+void UAsyncAction_ExperienceReady::Step4_BroadcastReady()
+{
+	OnReady.Broadcast();
+
+	SetReadyToDestroy();
+}
