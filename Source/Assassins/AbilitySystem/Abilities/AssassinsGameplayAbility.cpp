9bd78572e49165314cd7a31bdabbd43bc1cd0ee5
chickenchobab <hew980812@gmail.com>
2026-04-16 23:22:05 +0900

    OnGiveAbility에서 활성화되는 ability로 인한 에러 해결
    캐릭터의 Velocity가 서버에서는 정상적으로 반영되지만 클라이언트에서는 바로 0으로 돌아오는 문제 발견
    TryActivateAbility는 기본적으로 ability의 원격 활성화를 허용하는데, 이를 호출 전에 차단하여 문제 해결

--- Source/Assassins/AbilitySystem/Abilities/AssassinsGameplayAbility.cpp
+++ Source/Assassins/AbilitySystem/Abilities/AssassinsGameplayAbility.cpp
@@ -133,9 +133,18 @@ void UAssassinsGameplayAbility::TryActivateAbilityOnSpawn(const FGameplayAbility
         const AActor* AvatarActor = ActorInfo->AvatarActor.Get();
 
         // If avatar actor is torn off or about to die, don't try to activate until we get the new one.
-        if (ASC && AvatarActor && (AvatarActor->GetLifeSpan() <= 0.0f))
+        if (ASC && AvatarActor && !AvatarActor->GetTearOff() && (AvatarActor->GetLifeSpan() <= 0.0f))
         {
-            ASC->TryActivateAbility(Spec.Handle);
+            const bool bIsLocalExecution = (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::LocalPredicted) || (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::LocalOnly);
+            const bool bIsServerExecution = (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::ServerOnly) || (NetExecutionPolicy == EGameplayAbilityNetExecutionPolicy::ServerInitiated);
+
+            const bool bClientShouldActivate = ActorInfo->IsLocallyControlled() && bIsLocalExecution;
+            const bool bServerShouldActivate = ActorInfo->IsNetAuthority() && bIsServerExecution;
+
+            if (bClientShouldActivate || bServerShouldActivate)
+            {
+                ASC->TryActivateAbility(Spec.Handle);
+            }
         }
     }
 }
